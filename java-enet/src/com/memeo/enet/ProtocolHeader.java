62d4a74cbf43d93c9cff16df7bfac25cba47634c
Casey Marshall <casey.s.marshall@gmail.com>
2011-11-23 10:49:35 -0800

    Implemented.

--- java-enet/src/com/memeo/enet/ProtocolHeader.java
+++ java-enet/src/com/memeo/enet/ProtocolHeader.java
@@ -1,5 +1,39 @@
 package com.memeo.enet;
 
-public class ProtocolHeader {
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
+public class ProtocolHeader
+{
+	private ByteBuffer buffer;
+	
+	public ProtocolHeader(ByteBuffer buffer)
+	{
+		this.buffer = buffer.duplicate().order(ByteOrder.BIG_ENDIAN);
+	}
+	
+	public int length()
+	{
+		return 4;
+	}
+	
+	public int getPeerID()
+	{
+		return buffer.getShort(0) & 0xFFFF;
+	}
+	
+	public int getSentTime()
+	{
+		return buffer.getShort(2) & 0xFFFF;
+	}
+	
+	public void setPeerID(int peerID)
+	{
+		buffer.putShort(0, (short) peerID);
+	}
+	
+	public void setSentTime(int sentTime)
+	{
+		buffer.putShort(2, (short) sentTime);
+	}
 }
